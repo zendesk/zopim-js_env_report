2699ee7ace74cbd4cc9a12616b7eb3d05e25dca7
Timothee Groleau <tg@timotheegroleau.com>
2014-11-24 16:26:49 +0800

    Serve reporter straight from rawgit

--- js_env_bookmarklet.js
+++ js_env_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function() {
 	var script = document.createElement('script');
 	script.setAttribute('charset','utf-8');
 	script.async = true;
-	script.src   = '//tim.zopim.com/bin/js_env_tester.js?' + Math.random();
+	script.src   = 'https://cdn.rawgit.com/zopim/js_env_report/f182a5e983/js_env_tester.js';
 	script.type  = 'text/javascript';
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(script);
