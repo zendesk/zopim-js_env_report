ba00458b00d524e19dd79dffe0b964667c2f2b77
Timothee Groleau <tg@timotheegroleau.com>
2014-11-25 10:09:31 +0800

    Update commit id in bookmarklet

--- js_env_bookmarklet.js
+++ js_env_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function() {
 	var script = document.createElement('script');
 	script.setAttribute('charset','utf-8');
 	script.async = true;
-	script.src   = 'https://cdn.rawgit.com/zopim/js_env_report/f182a5e983/js_env_tester.js';
+	script.src   = 'https://cdn.rawgit.com/zopim/js_env_report/490aa51a65/js_env_tester.js';
 	script.type  = 'text/javascript';
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(script);
