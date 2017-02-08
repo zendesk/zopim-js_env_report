c9ce20c8e4357f4b8916caa602268f2167d95d36
Timothee Groleau <tgroleau@zendesk.com>
2017-02-08 11:40:48 +0800

    Update bookmarklet to point to latest commit

--- js_env_bookmarklet.js
+++ js_env_bookmarklet.js
@@ -2,7 +2,7 @@ javascript:(function() {
 	var script = document.createElement('script');
 	script.setAttribute('charset','utf-8');
 	script.async = true;
-	script.src   = 'https://cdn.rawgit.com/zopim/js_env_report/0386c48de4/js_env_tester.js';
+	script.src   = 'https://cdn.rawgit.com/zopim/js_env_report/aa5e14d8a6/js_env_tester.js';
 	script.type  = 'text/javascript';
 	var body = document.getElementsByTagName('body')[0];
 	body.appendChild(script);
